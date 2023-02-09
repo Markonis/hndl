14e3f17570632c969bdd75f8cc8028c88312ca2b
Marko Pavlovic <contact.pavlovic@gmail.com>
2023-02-09 15:05:01 +0100

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { listener } from "./listener";
 export { endpoint, router } from "./endpoint";
 export { service } from "./service";
+export { Endpoint, Request, Response, ErrorHandler, Logger } from "./types";
+export * as HttpStatus from "./status";
